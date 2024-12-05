1a5dad41c68b1ca0635b2e9018160619bb9fce2f
Nikita <nikitaloginovxxx1986@gmail.com>
2024-12-06 00:22:39 +0300

    l a v5

---
+++ public/imagesComponents/Icont.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from "react";
+
+export const JavaScript: React.FC = () => {
+    return (
+        <Image
+            src="/images/IconsJavascript.png"
+            alt="картинка"
+            width={20}
+            height={20}
+        />
+    )
+}
+
+export const HTMLCSS: React.FC = () => {
+    return (
+        <Image
+            src="/images/icons8-код-30.png"
+            alt="картинка"
+            width={20}
+            height={20}
+        />
+    )
+}
